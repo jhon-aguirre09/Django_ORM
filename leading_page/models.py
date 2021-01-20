e51a80d6a25bde91d1332ce10cc1e139b4cd6afd
Jhon <jhon.aguirre09@gmail.com>
2021-01-20 06:46:45 -0500

    Signals

--- leading_page/models.py
+++ leading_page/models.py
@@ -37,10 +37,35 @@ class SubCategoria(modelClass):
     def __str__(self):
         return "{}:{}".format(self.categoria.description, self.description)
 
-    def save(self):
+    def save(self, *args, **kwargs):
         self.description = self.description.upper()
-        super(Categoria, self).save()
+        super(Categoria, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "Sub Categorias"
         unique_together = ('categoria', 'description')
+
+class Unico(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if self.__class__.objects.count():
+            self.pk = self.__class__.objects.first().pk
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def truncate(cls):
+        from django.db import connection
+        with connection.cursor() as cursor:
+            cursor.execute('TRUNCATE TABLE "{0}" CASCADE'.format(cls.meta.db_table)) 
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+@receiver(post_save, sender=Categoria)
+def categoria_saved(sender, **kwargs):
+    print ("Categoria Guardada")
+
+@receiver(post_delete, sender=Categoria)
+def categoria_deleted(sender, **kwargs):
+    print("Categoria Eliminada")
